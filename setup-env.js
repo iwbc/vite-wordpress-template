b0b335b3c8173349602a8be26c485cf997cca9ee
iwbc <1209760+iwbc@users.noreply.github.com>
2025-07-10 17:36:00 +0900

    pluginsをコンテナからコピーする
    余計なファイルが多いのでシンボリックリンクはやめる

--- setup-env.js
+++ setup-env.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs';
-import os from 'node:os';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -12,6 +11,7 @@ const env = {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// コンテナハッシュを取得し.envとして書き出す
 const { stdout } = await execa('npm', ['run', 'wp-env', '--', 'install-path']);
 env.WP_ENV_CONTAINER_HASH = path.basename(stdout);
 
@@ -20,10 +20,11 @@ const output = Object.entries(env)
   .join('\n');
 fs.writeFileSync(path.resolve(__dirname, '.env'), output);
 
-const wpEnvDir = path.join(os.homedir(), '.wp-env', env.WP_ENV_CONTAINER_HASH);
-const wpDir = path.resolve(__dirname, 'wp', 'plugins');
+// プラグインをコンテナからコピー
+const containerPluginsDir = `${env.WP_ENV_CONTAINER_HASH}-wordpress-1:/var/www/html/wp-content/plugins`;
+const hostPluginsDir = path.resolve(__dirname, 'wp', 'plugins');
 
-if (fs.lstatSync(wpDir)) {
-  fs.rmSync(wpDir, { recursive: true, force: true });
+if (fs.existsSync(hostPluginsDir)) {
+  fs.rmdirSync(hostPluginsDir, { recursive: true, force: true });
 }
-fs.symlink(wpEnvDir, wpDir, 'dir', () => {});
+await execa('docker', ['cp', containerPluginsDir, hostPluginsDir]);
